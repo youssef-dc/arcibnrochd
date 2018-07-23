30b175f321f826d132b326a1125753a2fbef1499
unknown <b.youssef@digitalcore.ma>
2018-07-23 23:52:23 +0100

    [arc-000] - initialisation projet

---
+++ application/views/footer.php
@@ -0,0 +1,111 @@
+<footer class="black">
+    <div class="footer-container">
+        <div class="container">
+            <div class="row">
+                <div class="footer-content">
+
+                    <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 mb">
+                        <div class="footer-about-info">
+                            <h4> About Us </h4>
+                            <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+                                eiusmod tempor incid- idunt ut labore et dolore magna aliqua. </p>
+                            <ul>
+                                <li><i class="icon-location-1"></i>
+                                    <span>Florida 123 6th St. Melbourne, FL 32904 </span></li>
+                                <li><i class="icon-phone"></i>
+                                    <span>Phone: 800-879-6497</span></li>
+                                <li><i class="icon-mail"></i>
+                                    <span>Email: <a href="mailto:#">thx@yoursite.com</a></span></li>
+                            </ul>
+                        </div>
+                        <div class="social-link footer-social-link">
+                            <ul>
+                                <li><a href="https://www.tumblr.com/" class="circle c-1"><i
+                                        class="icon-tumblr "></i></a></li>
+                                <li><a href="https://www.facebook.com/" class="circle c-2"><i
+                                        class="icon-facebook "></i></a></li>
+                                <li><a href="https://www.pinterest.com/" class="circle c-3"><i
+                                        class="icon-pinterest "></i></a></li>
+                                <li><a href="https://vimeo.com/" class="circle c-4"><i class="icon-vimeo "></i></a></li>
+                            </ul>
+                        </div> <!-- footer-social-link -->
+                    </div>
+
+                    <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 mb">
+                        <h4> Our News </h4>
+                        <div class="news-1">
+                            <a href="blog-single.html">
+                                <img src="http://via.placeholder.com/68x68" alt="...">
+                            </a>
+                            <div class="news">
+                                <h5 class="news-h"><a href="blog-single.html">Water Isn&#8217;t All Africa Needs</a></h5>
+                                <p class="date">June 28, 2017</p>
+                            </div>
+                        </div>
+                        <div class="news-2">
+                            <a href="blog-single.html">
+                                <img src="http://via.placeholder.com/68x68" alt="...">
+                            </a>
+                            <div class="news">
+                                <h5 class="news-h"><a href="blog-single.html">Protection Is Not an Option &#8211; It&#8217;s an
+                                    Obligation</a></h5>
+                                <p class="date">June 28, 2017</p>
+                            </div>
+                        </div>
+                        <div class="news-3">
+                            <a href="blog-single.html">
+                                <img src="http://via.placeholder.com/68x68" alt="...">
+                            </a>
+                            <div class="news">
+                                <h5 class="news-h"><a href="blog-single.html">How Does the Bible Define Poverty?</a></h5>
+                                <p class="date">June 28, 2017</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 mb">
+                        <h4> Contact Us </h4>
+                        <form action="...">
+                            <div class="form-group">
+                                <input id="first_name" name="first_name" type="text" placeholder="Name"
+                                       class="form-control" required="">
+                            </div>
+                            <div class="form-group">
+                                <input id="email" name="email" type="email" placeholder="E-mail"
+                                       class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <textarea class="form-control" rows="4" placeholder="Message..."></textarea>
+                            </div>
+                            <div class="form-group buttons ">
+                                <input type="submit" class="red-btn red-btn-form" value="Send Message">
+                            </div>
+                        </form>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+        <div class="back-to-top">
+            <a class="top top-page" href="#"> <i class="icon-up-open-big"></i></a>
+        </div>
+
+        <div class="back-to-top-mob">
+            <a class="top top-mob" href="#"> <i class="icon-up-open-big"></i></a>
+        </div>
+    </div>
+
+    <div id="copyright">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <p class="copy-title">Copyright &copy; 2017 thx. Theme by
+                        <a href="https://fruitfulcode.com/" target="_blank">fruitfulcode</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</footer>
